ef3333a51393ae813fc80ae7ceebaeb53857cb8c
Игорь Кальченко <igorkalchenko1997@gmail.com>
2022-10-13 21:11:19 +0300

    Fix recipe update

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db.models import Sum
 from django.http import HttpResponse
 from recipes.models import RecipeIngredient
@@ -9,11 +11,12 @@ def download_cart(user):
     if not user.is_in_shopping_cart.exists():
         return Response(status=status.HTTP_400_BAD_REQUEST)
     ingredients = RecipeIngredient.objects.filter(
-        recipe__in=(user.is_in_shopping_cart.values('id'))
+        recipe__is_in_shopping_cart_user=user
     ).values(
         'ingredients__name',
         'ingredients__measurement_unit'
     ).annotate(amount=Sum('amount'))
+    logging.error(ingredients)
     filename = f'{user.username}_shopping_list.txt'
     shopping_list = 'Shopping List\n\n'
     for ingredient in ingredients:
