99e2e8f3b84c314f0fc3a3c5f90829a8e087be69
Игорь Кальченко <igorkalchenko1997@gmail.com>
2022-11-03 02:30:56 +0300

    Fix subscription mechanism

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -179,18 +179,19 @@ class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 '"Ingredients" field must be filled out.'
             )
-        ing_in_recipe = []
+        # ing_in_recipe = []
         for ing in data['ingredients']:
-            if int(ing['amount']) <= 0:
-                raise serializers.ValidationError(
-                    'Amount must be greater than 0'
-                )
-            ingredient = get_object_or_404(Ingredient, id=ing['id'])
-            if ingredient in ing_in_recipe:
-                raise serializers.ValidationError(
-                    f'You have repeated ingredients: {ingredient}'
-                )
-            ing_in_recipe.append(ingredient)
+            raise serializers.ValidationError(ing)
+            # if int(ing['amount']) <= 0:
+            #     raise serializers.ValidationError(
+            #         'Amount must be greater than 0'
+            #     )
+            # ingredient = get_object_or_404(Ingredient, id=ing['id'])
+            # if ingredient in ing_in_recipe:
+            #     raise serializers.ValidationError(
+            #         f'You have repeated ingredients: {ingredient}'
+            #     )
+            # ing_in_recipe.append(ingredient)
         return data
 
     @classmethod
